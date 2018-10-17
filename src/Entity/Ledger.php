22e057e069051a52fb5b6cf430866636b49004b6
iampersistent <shank@zestic.com>
2018-10-17 10:16:23 -0700

    add setters and getters to Ledger

--- src/Entity/Ledger.php
+++ src/Entity/Ledger.php
@@ -11,4 +11,42 @@ final class Ledger
     private $balance;
     /** @var Entry[] */
     private $entries;
+    /** @var int */
+    private $id;
+
+    public function getBalance(): Money
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(Money $balance): Ledger
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getEntries(): array
+    {
+        return $this->entries;
+    }
+
+    public function setEntries(array $entries): Ledger
+    {
+        $this->entries = $entries;
+
+        return $this;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Ledger
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
